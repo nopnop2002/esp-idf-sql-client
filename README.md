502473e7a238bc4cc4f8303d3e0a24020e357f74
nopnop2002 <hau26280@tree.odn.ne.jp>
2020-11-01 10:49:28 +0900

    Stopped using the ring buffer

--- README.md
+++ README.md
@@ -167,11 +167,13 @@ PASSWORD of your wifi.
 - CONFIG_ESP_MAXIMUM_RETRY   
 Maximum number of retries when connecting to wifi.
 - CONFIG_ESP_WEB_SERVER_IP   
-IP or DNS of WEB Server
+IP or DNS of your WEB Server.
 - CONFIG_ESP_WEB_SERVER_PORT   
-Port number of WEB Server
+Port number of your WEB Server.
 - CONFIG_ESP_PHP_PATH   
-Path of PHP Script
+Path of PHP Script,
+- CONFIG_JSON_PARSE   
+Enable JSON parse.
 
 ```
 git clone https://github.com/nopnop2002/esp-idf-mysql
@@ -180,130 +182,60 @@ make menuconfig
 make flash monitor
 ```
 
-![menuconfig-11](https://user-images.githubusercontent.com/6020549/71758309-4f3ae300-2ee1-11ea-86f6-89869ae35f63.jpg)
+![menuconfig-1](https://user-images.githubusercontent.com/6020549/97793281-68114380-1c2d-11eb-9787-c8df218693ed.jpg)
 
-![menuconfig-12](https://user-images.githubusercontent.com/6020549/71758645-033e6d00-2ee6-11ea-81b5-a3b9d954e9fe.jpg)
+![menuconfig-2](https://user-images.githubusercontent.com/6020549/97793282-69427080-1c2d-11eb-929f-2dce855073c3.jpg)
 
 ## Read all data
 ```
-I (35386) JSON: path=
-I (35386) JSON: url=http://192.168.10.43:8080/api.php/records/posts
-I (36666) JSON: HTTP GET Status = 200, content_length = 134
-I (37666) JSON: buffer=
+I (2929) JSON:
 {"records":[{"id":1,"user_id":1,"category_id":1,"content":"blog started"},{"id":2,"user_id":1,"category_id":2,"content":"It works!"}]}
-I (37666) JSON: Deserialize.....
-I (37666) JSON: [records]
-I (37676) JSON: [id]
-I (37676) JSON: int=1 double=1.000000
-I (37676) JSON: [user_id]
-I (37686) JSON: int=1 double=1.000000
-I (37686) JSON: [category_id]
-I (37686) JSON: int=1 double=1.000000
-I (37696) JSON: [content]
-I (37696) JSON: blog started
-I (37706) JSON: [id]
-I (37706) JSON: int=2 double=2.000000
-I (37706) JSON: [user_id]
-I (37716) JSON: int=1 double=1.000000
-I (37716) JSON: [category_id]
-I (37716) JSON: int=2 double=2.000000
-I (37726) JSON: [content]
-I (37726) JSON: It works!
+I (8902) JSON: Deserialize.....
+I (8902) JSON: [id] int=1 double=1.000000
+I (8912) JSON: [user_id] int=1 double=1.000000
+I (8912) JSON: [category_id] int=1 double=1.000000
+I (8922) JSON: [content] blog started
+I (8922) JSON: [id] int=2 double=2.000000
+I (8932) JSON: [user_id] int=1 double=1.000000
+I (8932) JSON: [category_id] int=2 double=2.000000
+I (8942) JSON: [content] It works!
 ```
 
 ## Read by ID
 ```
-I (70386) JSON: path=1
-I (70386) JSON: url=http://192.168.10.43:8080/api.php/records/posts/1
-I (70416) JSON: HTTP GET Status = 200, content_length = 61
-I (71416) JSON: buffer=
-{"id":1,"user_id":1,"category_id":1,"content":"blog started"}
-I (71416) JSON: Deserialize.....
-I (71416) JSON: [id]
-I (71416) JSON: int=1 double=1.000000
-I (71416) JSON: [user_id]
-I (71426) JSON: int=1 double=1.000000
-I (71426) JSON: [category_id]
-I (71436) JSON: int=1 double=1.000000
-I (71436) JSON: [content]
-I (71436) JSON: blog started
+I (24179) JSON:
+{"id":2,"user_id":1,"category_id":2,"content":"It works!"}
+I (27092) JSON: Deserialize.....
+I (27092) JSON: [id] int=2 double=2.000000
+I (27092) JSON: [user_id] int=1 double=1.000000
+I (27102) JSON: [category_id] int=2 double=2.000000
+I (27102) JSON: [content] It works!
 ```
 
 ## Create new record
 ```
-I (95486) JSON: url=http://192.168.10.43:8080/api.php/records/posts
-I (95656) JSON: HTTP GET Status = 200, content_length = 1
-I (96656) JSON: buffer=
-3
-I (96656) JSON: new_id=3
+I (37689) JSON:
+{"id":14,"user_id":1,"category_id":3,"content":"Hello World"}
+I (39452) JSON: Deserialize.....
+I (39452) JSON: [id] int=15 double=15.000000
+I (39452) JSON: [user_id] int=1 double=1.000000
+I (39462) JSON: [category_id] int=3 double=3.000000
+I (39462) JSON: [content] Hello World
 ```
 
 ## Update record
 ```
-I (115586) JSON: path=3
-I (115586) JSON: url=http://192.168.10.43:8080/api.php/records/posts/3
-I (115616) JSON: HTTP GET Status = 200, content_length = 60
-I (116616) JSON: buffer=
-{"id":3,"user_id":1,"category_id":3,"content":"Hello World"}
-I (116616) JSON: Deserialize.....
-I (116616) JSON: [id]
-I (116616) JSON: int=3 double=3.000000
-I (116616) JSON: [user_id]
-I (116626) JSON: int=1 double=1.000000
-I (116626) JSON: [category_id]
-I (116636) JSON: int=3 double=3.000000
-I (116636) JSON: [content]
-I (116636) JSON: Hello World
-I (116646) JSON: path=3
-I (116656) JSON: url=http://192.168.10.43:8080/api.php/records/posts/3
-I (116806) JSON: HTTP GET Status = 200, content_length = 1
-I (117806) JSON: buffer=
-1
-I (117806) JSON: path=3
-I (117806) JSON: url=http://192.168.10.43:8080/api.php/records/posts/3
-I (117836) JSON: HTTP GET Status = 200, content_length = 60
-I (118836) JSON: buffer=
-{"id":3,"user_id":1,"category_id":3,"content":"Hello Japan"}
-I (118836) JSON: Deserialize.....
-I (118836) JSON: [id]
-I (118836) JSON: int=3 double=3.000000
-I (118836) JSON: [user_id]
-I (118846) JSON: int=1 double=1.000000
-I (118846) JSON: [category_id]
-I (118856) JSON: int=3 double=3.000000
-I (118856) JSON: [content]
-I (118856) JSON: Hello Japan
+I (64519) JSON:
+{"id":14,"user_id":1,"category_id":3,"content":"Hello Japan"}
+I (51542) JSON: Deserialize.....
+I (51542) JSON: [id] int=15 double=15.000000
+I (51542) JSON: [user_id] int=1 double=1.000000
+I (51552) JSON: [category_id] int=3 double=3.000000
+I (51552) JSON: [content] Hello Japan
 ```
 
 ## Delete record
 ```
-I (149386) JSON: path=3
-I (149386) JSON: url=http://192.168.10.43:8080/api.php/records/posts/3
-I (149436) JSON: HTTP GET Status = 200, content_length = 60
-I (150436) JSON: buffer=
-{"id":3,"user_id":1,"category_id":3,"content":"Hello Japan"}
-I (150436) JSON: Deserialize.....
-I (150436) JSON: [id]
-I (150436) JSON: int=3 double=3.000000
-I (150436) JSON: [user_id]
-I (150446) JSON: int=1 double=1.000000
-I (150446) JSON: [category_id]
-I (150456) JSON: int=3 double=3.000000
-I (150456) JSON: [content]
-I (150456) JSON: Hello Japan
-I (150466) JSON: path=3
-I (150476) JSON: url=http://192.168.10.43:8080/api.php/records/posts/3
-I (150646) JSON: HTTP GET Status = 200, content_length = 1
-I (151646) JSON: buffer=
-1
-I (151646) JSON: path=3
-I (151646) JSON: url=http://192.168.10.43:8080/api.php/records/posts/3
-I (151686) JSON: HTTP GET Status = 404, content_length = 46
-I (152686) JSON: buffer=
-{"code":1003,"message":"Record '3' not found"}
-I (152686) JSON: Deserialize.....
-I (152686) JSON: [code]
-I (152686) JSON: int=1003 double=1003.000000
-I (152686) JSON: [message]
-I (152696) JSON: Record '3' not found
+I (84279) JSON:
+{"code":1003,"message":"Record '14' not found"}
 ```
--- main/main.c
+++ main/main.c
@@ -7,7 +7,6 @@
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
 #include "freertos/event_groups.h"
-#include "freertos/ringbuf.h"
 #include "esp_system.h"
 #include "esp_wifi.h"
 #include "esp_event.h"
@@ -46,8 +45,6 @@ static const char *TAG = "JSON";
 
 static int s_retry_num = 0;
 
-RingbufHandle_t xRingbuffer;
-
 EventGroupHandle_t xEventGroup;
 /* - Is the Enter key entered? */
 const int KEYBOARD_ENTER_BIT = BIT2;
@@ -55,9 +52,11 @@ const int KEYBOARD_ENTER_BIT = BIT2;
 
 esp_err_t _http_event_handler(esp_http_client_event_t *evt)
 {
+	static char *output_buffer;  // Buffer to store response of http request from event handler
+	static int output_len;		 // Stores number of bytes read
 	switch(evt->event_id) {
 		case HTTP_EVENT_ERROR:
-			ESP_LOGE(TAG, "HTTP_EVENT_ERROR");
+			ESP_LOGD(TAG, "HTTP_EVENT_ERROR");
 			break;
 		case HTTP_EVENT_ON_CONNECTED:
 			ESP_LOGD(TAG, "HTTP_EVENT_ON_CONNECTED");
@@ -70,24 +69,47 @@ esp_err_t _http_event_handler(esp_http_client_event_t *evt)
 			break;
 		case HTTP_EVENT_ON_DATA:
 			ESP_LOGD(TAG, "HTTP_EVENT_ON_DATA, len=%d", evt->data_len);
-			if (!esp_http_client_is_chunked_response(evt->client)) {
-				//char buffer[512];
-				char *buffer = malloc(evt->data_len + 1);
-				esp_http_client_read(evt->client, buffer, evt->data_len);
-				buffer[evt->data_len] = 0;
-				//ESP_LOGI(TAG, "buffer=%s", buffer);
-				UBaseType_t res = xRingbufferSend(xRingbuffer, buffer, evt->data_len, pdMS_TO_TICKS(1000));
-				if (res != pdTRUE) {
-					ESP_LOGW(TAG, "Failed to xRingbufferSend");
+			ESP_LOGD(TAG, "HTTP_EVENT_ON_DATA, output_len=%d", output_len);
+			ESP_LOGD(TAG, "HTTP_EVENT_ON_DATA, content_length=%d", esp_http_client_get_content_length(evt->client));
+			// If user_data buffer is configured, copy the response into the buffer
+			if (evt->user_data) {
+				memcpy(evt->user_data + output_len, evt->data, evt->data_len);
+			} else {
+				if (output_buffer == NULL && esp_http_client_get_content_length(evt->client) > 0) {
+					output_buffer = (char *) malloc(esp_http_client_get_content_length(evt->client));
+					output_len = 0;
+					if (output_buffer == NULL) {
+						ESP_LOGE(TAG, "Failed to allocate memory for output buffer");
+						return ESP_FAIL;
+					}
 				}
-				free(buffer);
+				memcpy(output_buffer + output_len, evt->data, evt->data_len);
 			}
+			output_len += evt->data_len;
 			break;
 		case HTTP_EVENT_ON_FINISH:
 			ESP_LOGD(TAG, "HTTP_EVENT_ON_FINISH");
+			if (output_buffer != NULL) {
+				// Response is accumulated in output_buffer. Uncomment the below line to print the accumulated response
+				// ESP_LOG_BUFFER_HEX(TAG, output_buffer, output_len);
+				free(output_buffer);
+				output_buffer = NULL;
+			}
+			output_len = 0;
 			break;
 		case HTTP_EVENT_DISCONNECTED:
 			ESP_LOGD(TAG, "HTTP_EVENT_DISCONNECTED");
+			int mbedtls_err = 0;
+			esp_err_t err = esp_tls_get_and_clear_last_error(evt->data, &mbedtls_err, NULL);
+			if (err != 0) {
+				if (output_buffer != NULL) {
+					free(output_buffer);
+					output_buffer = NULL;
+				}
+				output_len = 0;
+				ESP_LOGE(TAG, "Last esp error code: 0x%x", err);
+				ESP_LOGE(TAG, "Last mbedtls failure: 0x%x", mbedtls_err);
+			}
 			break;
 	}
 	return ESP_OK;
@@ -164,48 +186,53 @@ char *JSON_Types(int type) {
 	return NULL;
 }
 
-void JSON_Parse(const cJSON * const root) {
+void JSON_Analyze(const cJSON * const root) {
 	//ESP_LOGI(TAG, "root->type=%s", JSON_Types(root->type));
 	cJSON *current_element = NULL;
-	//ESP_LOGI(TAG, "roo->child=%p", root->child);
-	//ESP_LOGI(TAG, "roo->next =%p", root->next);
+	//ESP_LOGI(TAG, "root->child=%p", root->child);
+	//ESP_LOGI(TAG, "root->next =%p", root->next);
+	static char* string;
 	cJSON_ArrayForEach(current_element, root) {
 		//ESP_LOGI(TAG, "type=%s", JSON_Types(current_element->type));
 		//ESP_LOGI(TAG, "current_element->string=%p", current_element->string);
 		if (current_element->string) {
-			const char* string = current_element->string;
-			ESP_LOGI(TAG, "[%s]", string);
+			//const char* string = current_element->string;
+			string = current_element->string;
+			//ESP_LOGI(TAG, "[%s]", string);
 		}
 		if (cJSON_IsInvalid(current_element)) {
-			ESP_LOGI(TAG, "Invalid");
+			ESP_LOGW(TAG, "Invalid");
 		} else if (cJSON_IsFalse(current_element)) {
-			ESP_LOGI(TAG, "False");
+			ESP_LOGI(TAG, "[%s] False", string);
 		} else if (cJSON_IsTrue(current_element)) {
-			ESP_LOGI(TAG, "True");
+			ESP_LOGI(TAG, "[%s] True", string);
 		} else if (cJSON_IsNull(current_element)) {
-			ESP_LOGI(TAG, "Null");
+			ESP_LOGI(TAG, "[%s] Null", string);
 		} else if (cJSON_IsNumber(current_element)) {
 			int valueint = current_element->valueint;
 			double valuedouble = current_element->valuedouble;
-			ESP_LOGI(TAG, "int=%d double=%f", valueint, valuedouble);
+			ESP_LOGI(TAG, "[%s] int=%d double=%f", string, valueint, valuedouble);
 		} else if (cJSON_IsString(current_element)) {
 			const char* valuestring = current_element->valuestring;
-			ESP_LOGI(TAG, "%s", valuestring);
+			ESP_LOGI(TAG, "[%s] %s", string, valuestring);
 		} else if (cJSON_IsArray(current_element)) {
 			//ESP_LOGI(TAG, "Array");
-			JSON_Parse(current_element);
+			JSON_Analyze(current_element);
 		} else if (cJSON_IsObject(current_element)) {
 			//ESP_LOGI(TAG, "Object");
-			JSON_Parse(current_element);
+			JSON_Analyze(current_element);
 		} else if (cJSON_IsRaw(current_element)) {
-			ESP_LOGI(TAG, "Raw(Not support)");
+			ESP_LOGW(TAG, "Raw(Not support)");
 		}
 	}
 }
 
+#define MAX_HTTP_OUTPUT_BUFFER 2048
+
 esp_err_t http_client_get(char * path)
 {
-	ESP_LOGI(TAG, "path=%s",path);
+	ESP_LOGI(TAG, "http_client_get path=%s",path);
+	char local_response_buffer[MAX_HTTP_OUTPUT_BUFFER] = {0};
 	char url[64];
 	//http://192.168.10.43:8080/api.php/records/posts
 	sprintf(url, "http://%s:%s/%s", EXAMPLE_ESP_WEB_SERVER_IP, EXAMPLE_ESP_WEB_SERVER_PORT, EXAMPLE_ESP_PHP_PATH);
@@ -217,115 +244,63 @@ esp_err_t http_client_get(char * path)
 	ESP_LOGI(TAG, "url=%s",url);
 	
 	esp_http_client_config_t config = {
-		//.url = "http://192.168.10.43:3000/todos",
 		.url = url,
 		.event_handler = _http_event_handler,
+		.user_data = local_response_buffer,		   // Pass address of local buffer to get response
 	};
 	esp_http_client_handle_t client = esp_http_client_init(&config);
 
 	// GET
-	esp_err_t ret;
 	esp_err_t err = esp_http_client_perform(client);
 	if (err == ESP_OK) {
-		ESP_LOGI(TAG, "HTTP GET Status = %d, content_length = %d",
+		ESP_LOGD(TAG, "HTTP GET Status = %d, content_length = %d",
 				esp_http_client_get_status_code(client),
 				esp_http_client_get_content_length(client));
-		//Receive an item from no-split ring buffer
-		int bufferSize = esp_http_client_get_content_length(client);
-		char *buffer = malloc(bufferSize + 1); 
-		size_t item_size;
-		int	index = 0;
-		while (1) {
-			char *item = (char *)xRingbufferReceive(xRingbuffer, &item_size, pdMS_TO_TICKS(1000));
-			if (item != NULL) {
-				for (int i = 0; i < item_size; i++) {
-					//printf("%c", item[i]);
-					buffer[index] = item[i];
-					index++;
-					buffer[index] = 0;
-				}
-				//printf("\n");
-				//Return Item
-				vRingbufferReturnItem(xRingbuffer, (void *)item);
-			} else {
-				//Failed to receive item
-				ESP_LOGD(TAG, "End of receive item");
-				break;
-			}
-		}
-		ESP_LOGI(TAG, "buffer=\n%s", buffer);
+		ESP_LOGI(TAG, "\n%s", local_response_buffer);
 
+#if CONFIG_JSON_PARSE
 		ESP_LOGI(TAG, "Deserialize.....");
-		cJSON *root = cJSON_Parse(buffer);
-		JSON_Parse(root);
+		cJSON *root = cJSON_Parse(local_response_buffer);
+		JSON_Analyze(root);
 		cJSON_Delete(root);
-		free(buffer);
-		ret = ESP_OK;
-
+#endif
 	} else {
 		ESP_LOGE(TAG, "HTTP GET request failed: %s", esp_err_to_name(err));
-		ret = ESP_FAIL;
 	}
 	esp_http_client_cleanup(client);
-	return ret;
+	return err;
 }
 
 int http_client_post(void)
 {
+	char local_response_buffer[MAX_HTTP_OUTPUT_BUFFER] = {0};
 	char url[64];
 	//http://192.168.10.43:8080/api.php/records/posts
 	sprintf(url, "http://%s:%s/%s", EXAMPLE_ESP_WEB_SERVER_IP, EXAMPLE_ESP_WEB_SERVER_PORT, EXAMPLE_ESP_PHP_PATH);
 	ESP_LOGI(TAG, "url=%s",url);
 
 	esp_http_client_config_t config = {
-		//.url = "http://192.168.10.43:3000/todos",
 		.url = url,
 		.event_handler = _http_event_handler,
+		.user_data = local_response_buffer,				   // Pass address of local buffer to get response
 	};
 	esp_http_client_handle_t client = esp_http_client_init(&config);
 
 	// POST
 	long id = 0;
-	//const char *post_data = "field1=value1&field2=value2";
 	const char *post_data = "user_id=1&category_id=3&content=Hello World";
-	//esp_http_client_set_url(client, "http://httpbin.org/post");
 	esp_http_client_set_url(client, url);
 	esp_http_client_set_method(client, HTTP_METHOD_POST);
 	esp_http_client_set_post_field(client, post_data, strlen(post_data));
 	esp_err_t err = esp_http_client_perform(client);
 	if (err == ESP_OK) {
-		ESP_LOGI(TAG, "HTTP GET Status = %d, content_length = %d",
+		ESP_LOGD(TAG, "HTTP GET Status = %d, content_length = %d",
 				esp_http_client_get_status_code(client),
 				esp_http_client_get_content_length(client));
-		//Receive an item from no-split ring buffer
-		int bufferSize = esp_http_client_get_content_length(client);
-		char *buffer = malloc(bufferSize + 1);
-		size_t item_size;
-		int index = 0;
-		while (1) {
-			char *item = (char *)xRingbufferReceive(xRingbuffer, &item_size, pdMS_TO_TICKS(1000));
-			if (item != NULL) {
-				for (int i = 0; i < item_size; i++) {
-					//printf("%c", item[i]);
-					buffer[index] = item[i];
-					index++;
-					buffer[index] = 0;
-				}
-				//printf("\n");
-				//Return Item
-				vRingbufferReturnItem(xRingbuffer, (void *)item);
-			} else {
-				//Failed to receive item
-				ESP_LOGD(TAG, "End of receive item");
-				break;
-			}
-		}
-		ESP_LOGI(TAG, "buffer=\n%s", buffer);
-		id = strtol(buffer, NULL, 10);
-		free(buffer);
-
+		ESP_LOGI(TAG, "\n%s", local_response_buffer);
+		id = strtol(local_response_buffer, NULL, 10);
 	} else {
-		ESP_LOGE(TAG, "HTTP GET request failed: %s", esp_err_to_name(err));
+		ESP_LOGE(TAG, "HTTP POST request failed: %s", esp_err_to_name(err));
 	}
 	esp_http_client_cleanup(client);
 	return (int)id;
@@ -333,133 +308,138 @@ int http_client_post(void)
 
 esp_err_t http_client_put(char * path)
 {
-	ESP_LOGI(TAG, "path=%s", path);
+	ESP_LOGI(TAG, "http_client_put path=%s", path);
+	char local_response_buffer[MAX_HTTP_OUTPUT_BUFFER] = {0};
 	char url[64];
 	//http://192.168.10.43:8080/api.php/records/posts
 	sprintf(url, "http://%s:%s/%s", EXAMPLE_ESP_WEB_SERVER_IP, EXAMPLE_ESP_WEB_SERVER_PORT, EXAMPLE_ESP_PHP_PATH);
+	if (strlen(path) > 0) {
+		int url_length = strlen(url);
+		if (url[url_length-1] != '/') strcat(url,"/");
+		strcat(url, path);
+	}
 	ESP_LOGD(TAG, "url=%s",url);
 	
 	esp_http_client_config_t config = {
-		//.url = "http://192.168.10.43:3000/todos",
 		.url = url,
 		.event_handler = _http_event_handler,
+		.user_data = local_response_buffer,		   // Pass address of local buffer to get response
 	};
 	esp_http_client_handle_t client = esp_http_client_init(&config);
 
 	// PUT
-	esp_err_t ret;
-	int url_length = strlen(url);
-	if (url[url_length-1] != '/') strcat(url,"/");
-	strcat(url, path);
-	ESP_LOGI(TAG, "url=%s",url);
 	const char *post_data = "content=Hello Japan";
 	esp_http_client_set_url(client, url);
 	esp_http_client_set_method(client, HTTP_METHOD_PUT);
 	esp_http_client_set_post_field(client, post_data, strlen(post_data));
 	esp_err_t err = esp_http_client_perform(client);
 	if (err == ESP_OK) {
-		ESP_LOGI(TAG, "HTTP GET Status = %d, content_length = %d",
+		ESP_LOGD(TAG, "HTTP GET Status = %d, content_length = %d",
 				esp_http_client_get_status_code(client),
 				esp_http_client_get_content_length(client));
-		//Receive an item from no-split ring buffer
-		int bufferSize = esp_http_client_get_content_length(client);
-		char *buffer = malloc(bufferSize + 1);
-		size_t item_size;
-		int index = 0;
-		while (1) {
-			char *item = (char *)xRingbufferReceive(xRingbuffer, &item_size, pdMS_TO_TICKS(1000));
-			if (item != NULL) {
-				for (int i = 0; i < item_size; i++) {
-					//printf("%c", item[i]);
-					buffer[index] = item[i];
-					index++;
-					buffer[index] = 0;
-				}
-				//printf("\n");
-				//Return Item
-				vRingbufferReturnItem(xRingbuffer, (void *)item);
-			} else {
-				//Failed to receive item
-				ESP_LOGD(TAG, "End of receive item");
-				break;
-			}
-		}
-		ESP_LOGI(TAG, "buffer=\n%s", buffer);
-		ret = ESP_OK;
-		free(buffer);
-
+		ESP_LOGI(TAG, "\n%s", local_response_buffer);
 	} else {
-		ESP_LOGE(TAG, "HTTP GET request failed: %s", esp_err_to_name(err));
-		ret = ESP_FAIL;
+		ESP_LOGE(TAG, "HTTP PUT request failed: %s", esp_err_to_name(err));
 	}
 	esp_http_client_cleanup(client);
-	return ret;
+	return err;
 }
 
 esp_err_t http_client_delete(char * path)
 {
-	ESP_LOGI(TAG, "path=%s", path);
+	ESP_LOGI(TAG, "http_client_delete path=%s", path);
+	char local_response_buffer[MAX_HTTP_OUTPUT_BUFFER] = {0};
 	char url[64];
 	//http://192.168.10.43:8080/api.php/records/posts
 	sprintf(url, "http://%s:%s/%s", EXAMPLE_ESP_WEB_SERVER_IP, EXAMPLE_ESP_WEB_SERVER_PORT, EXAMPLE_ESP_PHP_PATH);
+	if (strlen(path) > 0) {
+		int url_length = strlen(url);
+		if (url[url_length-1] != '/') strcat(url,"/");
+		strcat(url, path);
+	}
 	ESP_LOGD(TAG, "url=%s",url);
 
 	esp_http_client_config_t config = {
-		//.url = "http://192.168.10.43:3000/todos",
 		.url = url,
 		.event_handler = _http_event_handler,
+		.user_data = local_response_buffer,		   // Pass address of local buffer to get response
 	};
 	esp_http_client_handle_t client = esp_http_client_init(&config);
 
 	// DELETE
-	esp_err_t ret;
-	int url_length = strlen(url);
-	if (url[url_length-1] != '/') strcat(url,"/");
-	strcat(url, path);
-	ESP_LOGI(TAG, "url=%s",url);
+	//int url_length = strlen(url);
+	//if (url[url_length-1] != '/') strcat(url,"/");
+	//strcat(url, path);
+	//ESP_LOGI(TAG, "url=%s",url);
 	esp_http_client_set_url(client, url);
 	esp_http_client_set_method(client, HTTP_METHOD_DELETE);
 	esp_err_t err = esp_http_client_perform(client);
 	if (err == ESP_OK) {
-		ESP_LOGI(TAG, "HTTP GET Status = %d, content_length = %d",
+		ESP_LOGD(TAG, "HTTP GET Status = %d, content_length = %d",
 				esp_http_client_get_status_code(client),
 				esp_http_client_get_content_length(client));
-		//Receive an item from no-split ring buffer
-		int bufferSize = esp_http_client_get_content_length(client);
-		char *buffer = malloc(bufferSize + 1);
-		size_t item_size;
-		int index = 0;
-		while (1) {
-			char *item = (char *)xRingbufferReceive(xRingbuffer, &item_size, pdMS_TO_TICKS(1000));
-			if (item != NULL) {
-				for (int i = 0; i < item_size; i++) {
-					//printf("%c", item[i]);
-					buffer[index] = item[i];
-					index++;
-					buffer[index] = 0;
-				}
-				//printf("\n");
-				//Return Item
-				vRingbufferReturnItem(xRingbuffer, (void *)item);
-			} else {
-				//Failed to receive item
-				ESP_LOGD(TAG, "End of receive item");
-				break;
-			}
-		}
-		ESP_LOGI(TAG, "buffer=\n%s", buffer);
-		ret = ESP_OK;
-		free(buffer);
-
+		ESP_LOGI(TAG, "\n%s", local_response_buffer);
 	} else {
-		ESP_LOGE(TAG, "HTTP GET request failed: %s", esp_err_to_name(err));
-		ret = ESP_FAIL;
+		ESP_LOGE(TAG, "HTTP DELETE request failed: %s", esp_err_to_name(err));
 	}
 	esp_http_client_cleanup(client);
-	return ret;
+	return err;
 }
 
-void keyin(void *pvParameters)
+void http_task(void *pvParameters)
+{
+	//Read all data
+	ESP_LOGI(TAG, "");
+	ESP_LOGI(TAG, "Enter key to Read all data");
+	xEventGroupClearBits(xEventGroup, KEYBOARD_ENTER_BIT);
+	xEventGroupWaitBits(xEventGroup, KEYBOARD_ENTER_BIT, pdFALSE, pdTRUE, portMAX_DELAY);
+	http_client_get("");
+
+	//Read by id
+	ESP_LOGI(TAG, "");
+	ESP_LOGI(TAG, "Enter key to Read by id");
+	xEventGroupClearBits(xEventGroup, KEYBOARD_ENTER_BIT);
+	xEventGroupWaitBits(xEventGroup, KEYBOARD_ENTER_BIT, pdFALSE, pdTRUE, portMAX_DELAY);
+	http_client_get("2");
+
+	//Create new record
+	ESP_LOGI(TAG, "");
+	ESP_LOGI(TAG, "Enter key to Create new record");
+	xEventGroupClearBits(xEventGroup, KEYBOARD_ENTER_BIT);
+	xEventGroupWaitBits(xEventGroup, KEYBOARD_ENTER_BIT, pdFALSE, pdTRUE, portMAX_DELAY);
+	int new_id = http_client_post();
+	ESP_LOGI(TAG, "new_id=%d", new_id);
+	char path[10];
+	sprintf(path, "%d", new_id);
+	http_client_get(path);
+
+	//Update record
+	ESP_LOGI(TAG, "");
+	ESP_LOGI(TAG, "Enter key to Update new record");
+	xEventGroupClearBits(xEventGroup, KEYBOARD_ENTER_BIT);
+	xEventGroupWaitBits(xEventGroup, KEYBOARD_ENTER_BIT, pdFALSE, pdTRUE, portMAX_DELAY);
+	if (http_client_get(path) == ESP_OK) {
+		http_client_put(path);
+		http_client_get(path);
+	}
+
+	//Delete record
+	ESP_LOGI(TAG, "");
+	ESP_LOGI(TAG, "Enter key to Delete new record");
+	xEventGroupClearBits(xEventGroup, KEYBOARD_ENTER_BIT);
+	xEventGroupWaitBits(xEventGroup, KEYBOARD_ENTER_BIT, pdFALSE, pdTRUE, portMAX_DELAY);
+	if (http_client_get(path) == ESP_OK) {
+		http_client_delete(path);
+		http_client_get(path);
+	}
+
+	while(1) {
+		vTaskDelay(1);
+	}
+}
+
+
+void keyin_task(void *pvParameters)
 {
 	//ESP_LOGI(pcTaskGetTaskName(0), "Start");
 	uint16_t c;
@@ -490,65 +470,15 @@ void app_main()
 	ESP_LOGI(TAG, "ESP_WIFI_MODE_STA");
 	wifi_init_sta();
 
-	//Create Ring Buffer 
-	//No Split
-	xRingbuffer = xRingbufferCreate(1024*10, RINGBUF_TYPE_NOSPLIT);
-	//Allow_Split
-	//xRingbuffer = xRingbufferCreate(1024*10, RINGBUF_TYPE_ALLOWSPLIT);
-
 	//Create EventGroup
 	xEventGroup = xEventGroupCreate();
 
 	//Check everything was created
-	configASSERT( xRingbuffer );
 	configASSERT( xEventGroup );
 
 	//Start keyboard task
-	xTaskCreate(keyin, "KEYIN", 1024*2, NULL, 2, NULL);
+	xTaskCreate(keyin_task, "KEYIN", 1024*2, NULL, 2, NULL);
 
-	//List
-	ESP_LOGI(TAG, "");
-	ESP_LOGI(TAG, "Enter key to List");
-	xEventGroupClearBits(xEventGroup, KEYBOARD_ENTER_BIT);
-	xEventGroupWaitBits(xEventGroup, KEYBOARD_ENTER_BIT, pdFALSE, pdTRUE, portMAX_DELAY);
-	http_client_get(""); 
-
-	//Read
-	ESP_LOGI(TAG, "");
-	ESP_LOGI(TAG, "Enter key to Read");
-	xEventGroupClearBits(xEventGroup, KEYBOARD_ENTER_BIT);
-	xEventGroupWaitBits(xEventGroup, KEYBOARD_ENTER_BIT, pdFALSE, pdTRUE, portMAX_DELAY);
-	http_client_get("1"); 
-
-	//Create
-	ESP_LOGI(TAG, "");
-	ESP_LOGI(TAG, "Enter key to Create");
-	xEventGroupClearBits(xEventGroup, KEYBOARD_ENTER_BIT);
-	xEventGroupWaitBits(xEventGroup, KEYBOARD_ENTER_BIT, pdFALSE, pdTRUE, portMAX_DELAY);
-	int new_id = http_client_post(); 
-	ESP_LOGI(TAG, "new_id=%d", new_id);
-
-	//Update
-	ESP_LOGI(TAG, "");
-	ESP_LOGI(TAG, "Enter key to Update");
-	xEventGroupClearBits(xEventGroup, KEYBOARD_ENTER_BIT);
-	xEventGroupWaitBits(xEventGroup, KEYBOARD_ENTER_BIT, pdFALSE, pdTRUE, portMAX_DELAY);
-	char path[10];
-	sprintf(path, "%d", new_id);
-	ret = http_client_get(path); 
-	if (ret == ESP_OK) {
-		http_client_put(path); 
-		http_client_get(path); 
-	}
-
-	//Delete
-	ESP_LOGI(TAG, "");
-	ESP_LOGI(TAG, "Enter key to Delete");
-	xEventGroupClearBits(xEventGroup, KEYBOARD_ENTER_BIT);
-	xEventGroupWaitBits(xEventGroup, KEYBOARD_ENTER_BIT, pdFALSE, pdTRUE, portMAX_DELAY);
-	ret = http_client_get(path); 
-	if (ret == ESP_OK) {
-		http_client_delete(path); 
-		http_client_get(path); 
-	}
+	//Start http task
+	xTaskCreate(http_task, "HTTP", 1024*8, NULL, 2, NULL);
 }
